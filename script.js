92f6b8a9d0b1318ea461b1da412f2a185e47a7bb
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-21 20:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const timerElement = document.getElementById('timer');
+function updateTimer() {
+	const now = new Date();
+	const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+	const formattedTime = now.toLocaleDateString('en-US', options);
+	timerElement.textContent = formattedTime;
+}
+updateTimer();
+setInterval(updateTimer, 1000);
